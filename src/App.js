947d8bfcb3430f505417b7c68d078b2d9774b9f1
jinny(윤유진) <jinny.youn@dktechin.com>
2020-12-18 00:59:59 +0900

    [setting] - react init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Page } from "./pages/Page";
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      원투쓰리포...
+      <Page />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
