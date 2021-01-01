fd182521c98afedb74e28357f58d1e4bc55e5ad4
Youijn <constjinny@gmail.com>
2021-01-01 16:26:15 +0900

    [merge] main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import { Route } from "react-router-dom";
 import styled from "@emotion/styled";
-<<<<<<< HEAD
 import { Header, Footer, Home, Feed, My, Login } from "./pages";
 import { Button, SelectBox } from "./components";
-=======
-import { Header, Footer, Home, Feed, My } from "./pages";
 // import { Button, SelectBox } from "./components";
->>>>>>> 5df366fc53b77af6a2002d676318c7f00d2bf0ce
 
 function App() {
   return (
     <AppLayout>
       <Header />
-      {/* <Test>
+      <Test>
         <h1>test</h1>
         <Button />
         <SelectBox />
-      </Test> */}
+      </Test>
       <Route exact path="/" component={Home} />
       <Route path="/feed" component={Feed} />
       <Route path="/my" component={My} />
